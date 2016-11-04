5fad0183797c30511b09acb75f210a1d50a5b959
Bart Cornelis <bart.cornelis@gmail.com>
2016-11-04 16:19:11 +0100

    Update

--- deployment_scripts/deploy.sh
+++ deployment_scripts/deploy.sh
@@ -5,6 +5,7 @@ mkdir -p /usr/share/openstack-dashboard/openstack_dashboard/themes/stackurity/st
 
 # copy the logo files
 cp logos/*.png /usr/share/openstack-dashboard/openstack_dashboard/themes/stackurity/static/img
+cp logos/favicon.ico /usr/share/openstack-dashboard/openstack_dashboard/themes/stackurity/static/img
 
 # create the file to overwrite the themes
 echo "AVAILABLE_THEMES = [('stackurity', 'Stackurity', 'themes/stackurity')]" > /usr/share/openstack-dashboard/openstack_dashboard/local/local_settings.d/_11_set_default_theme.py
