0f1163390ee2759c9616971f40c5da714981001b
Bart Cornelis <bart.cornelis@gmail.com>
2016-11-04 15:47:07 +0100

    Create the first version of the deployment script

--- deployment_scripts/deploy.sh
+++ deployment_scripts/deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-# It's a script which deploys your plugin
-echo fuel-stackurity-theme > /tmp/fuel-stackurity-theme
+# create the required directories
+mkdir -p /usr/share/openstack-dashboard/openstack_dashboard/themes/stackurity/static/img
+
+# copy the logo files
+cp logos/*.png /usr/share/openstack-dashboard/openstack_dashboard/themes/stackurity/static/img
+
+# create the file to overwrite the themes
+echo "AVAILABLE_THEMES = [('stackurity', 'Stackurity', 'themes/stackurity')]" > /usr/share/openstack-dashboard/openstack_dashboard/local/local_settings.d/_11_set_default_theme.py
+
+# recompile
+/usr/share/openstack-dashboard/manage.py compress --force 
+/usr/share/openstack-dashboard/manage.py collectstatic 
+
+# and reload apache
+service apache2 reload
